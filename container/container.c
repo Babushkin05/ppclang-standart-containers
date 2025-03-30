a507b191f20d25ddade430234da497a43380604a
babushkin05 <babushkinvova05@gmail.com>
2025-03-30 23:01:48 +0300

    edit container base

---
+++ container/container.c
@@ -0,0 +1,30 @@
+#include "container.h"
+
+// Container Initialisation
+void InitContainer<Container* container>() {}
+
+// Container Initialisation with size parameter
+void InitContainerWithSize<Container* container>(size_t size) {}
+
+// Deleting all elements of container
+void ClearContainer<Container* container>() {}
+
+// Checking is container empty
+_Bool EmptyContainer<Container* container>() {}
+
+// --------------------------------------------
+// Iterators
+
+// Iterator for index (only for RandomAccesContainers)
+Value** ArrayAt<Container* container>(int index) {
+    return NULL;
+};
+
+// Iterator to first element
+Value** BeginContainer<Container* container>() {}
+
+// Iterator to last element
+Value** EndContainer<Container* container>() {}
+
+// Move iterator to next element in container
+void NextIteratorContainer<Container* container>(Value*** iter) {}
---
+++ container/container.h
@@ -0,0 +1,36 @@
+#ifndef __container__
+#define __container__
+
+#include "../value/value.c"
+
+// Generalised container structure
+typedef struct Container{}<> Container;
+
+// Container Initialisation
+void InitContainer<Container* container>();
+
+// Container Initialisation with size parameter
+void InitContainerWithSize<Container* container>(size_t size);
+
+// Deleting all elements of container
+void ClearContainer<Container* container>();
+
+// Checking is container empty
+_Bool EmptyContainer<Container* container>();
+
+// --------------------------------------------
+// Iterators
+
+// Iterator for index (only for RandomAccesContainers)
+Value** ArrayAt<Container* container>(int index);
+
+// Iterator to first element
+Value** BeginContainer<Container* container>();
+
+// Iterator to last element
+Value** EndContainer<Container* container>();
+
+// Move iterator to next element in container
+void NextIteratorContainer<Container* container>(Value*** iter);
+
+#endif // __container__
