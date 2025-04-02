4d443f583c78a45cb528c3ea60b2a4578117c3fe
babushkin05 <babushkinvova05@gmail.com>
2025-04-02 14:40:58 +0300

    reformat stack

--- stack/stack.h
+++ stack/stack.h
@@ -4,28 +4,27 @@
 #include "../value/value.c"
 
 typedef struct Stack {
-    int capasity;
-    uint topInd;
-    Value** data;
+  int capasity;
+  uint topInd;
+  Value **data;
 } Stack;
 
-
 // Initialisation
-void InitStack(Stack* stack);
+void InitStack(Stack *stack);
 
 // Get top of the stack
-Value* StackTop(Stack* stack);
+Value *StackTop(Stack *stack);
 
 // Push element to stack
-void StackPush(Stack* stack, Value* value);
+void StackPush(Stack *stack, Value *value);
 
 // Pop element from pop of the stack
-Value* StackPop(Stack* stack);
+Value *StackPop(Stack *stack);
 
 // Clear array;
-void ClearStack(Stack* stack);
+void ClearStack(Stack *stack);
 
 // Check is stack empty
-_Bool StackEmpty(Stack* stack);
+_Bool StackEmpty(Stack *stack);
 
 #endif // __stack
