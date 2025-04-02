f32bd32a8d0664c85f64d7db6258fa942316abd0
babushkin05 <babushkinvova05@gmail.com>
2025-04-02 14:42:19 +0300

    reformat container

--- container/container.c
+++ container/container.c
@@ -1,39 +1,38 @@
 #include "container.h"
 
 // Container Initialisation
-void InitContainer<Container* container>() {}
+void InitContainer<Container * container>() {}
 
 // Container Initialisation with size parameter
-void InitContainerWithSize<Container* container>(size_t size) {}
+void InitContainerWithSize<Container * container>(size_t size) {}
 
 // Deleting all elements of container
-void ClearContainer<Container* container>() {}
+void ClearContainer<Container * container>() {}
 
 // Checking is container empty
-_Bool EmptyContainer<Container* container>() {}
+_Bool EmptyContainer<Container * container>() {}
 
-void PushContainer<Container* container>(Value* val) {}
+void PushContainer<Container * container>(Value *val) {}
 
-void PushBackContainer<Container* container>(Value* val) {}
+void PushBackContainer<Container * container>(Value *val) {}
 
-Value* PopContainer<Container* container>() {}
+Value *PopContainer<Container * container>() {}
 
-void PopBackContainer<Container* container>() {}
+void PopBackContainer<Container * container>() {}
 
-Value* TopContainer<Container* container>() {}
-
+Value *TopContainer<Container * container>() {}
 
 // --------------------------------------------
 // Iterators
 
 // Iterator for index (only for RandomAccesContainers)
-Value** ContainerAt<Container* container>(int index) {};
+Value **ContainerAt<Container * container>(int index) {};
 
 // Iterator to first element
-Value** BeginContainer<Container* container>() {}
+Value **BeginContainer<Container * container>() {}
 
 // Iterator to last element
-Value** EndContainer<Container* container>() {}
+Value **EndContainer<Container * container>() {}
 
 // Move iterator to next element in container
-void NextIteratorContainer<Container* container>(Value*** iter) {}
+void NextIteratorContainer<Container * container>(Value ***iter) {}
