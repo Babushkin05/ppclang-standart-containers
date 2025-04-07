e65b383d4ca23a46d00f1835b63c0d1aa55d2f1e
babushkin05 <babushkinvova05@gmail.com>
2025-04-07 11:03:45 +0300

    header

---
+++ hashmap/hashmap.h
@@ -0,0 +1,29 @@
+#ifndef __hashmap__
+#define __hashmap__
+// hashmap.h
+
+typedef struct HashMapNode {
+    Value *key;
+    Value *val;
+    struct HashMapNode *next;
+} HashMapNode;
+
+typedef struct HashMap {
+    HashMapNode **buckets;
+    unsigned int size;
+    unsigned int cap;
+} HashMap;
+
+void InitHashMap(HashMap *map);
+
+_Bool HashMapEmpty(HashMap *map);
+
+void HashMapClear(HashMap *map);
+
+void HashMapInsert(HashMap *map, Value* key, Value *val);
+
+Value *HashMapAt(HashMap *map, Value *key);
+
+void HashMapErase(HashMap *map, Value *key);
+
+#endif // __hashmap__
